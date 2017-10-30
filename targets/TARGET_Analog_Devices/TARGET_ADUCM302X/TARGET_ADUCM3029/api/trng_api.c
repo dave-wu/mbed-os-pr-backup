8fc529b4559be449fb6739e1fe8ebb7f17db3c3d
Wu <dave.wu@analog.com>
2017-10-30 02:25:08 +0100

    - Added code to clear stack variable in trng_get_bytes upon exiting the function; - Remove lp ticker functionality as the timer hardware does not satisfy lp timer requirements.

--- targets/TARGET_Analog_Devices/TARGET_ADUCM302X/TARGET_ADUCM3029/api/trng_api.c
+++ targets/TARGET_Analog_Devices/TARGET_ADUCM302X/TARGET_ADUCM3029/api/trng_api.c
@@ -111,6 +111,9 @@ int trng_get_bytes(trng_t *obj, uint8_t *output, size_t length, size_t *output_l
 
         // Save the output
         output[i] = (uint8_t)(nRandomNum & 0xFF);
+        
+        // Clear nRandomNum on the stack before exiting
+        nRandomNum = 0;
     }
 
     *output_length = length;
